cd4cc5352563b5483bf7419632c34e99ec8a1734
Sarulian <tedjamesrobinson@gmail.com>
2017-03-29 20:12:44 -0700

    filling out new core logic

--- example/DE5-Net/fpga/rtl/fpga_core.v
+++ example/DE5-Net/fpga/rtl/fpga_core.v
@@ -71,4 +71,142 @@ module fpga_core
     input  wire [7:0]  sfp_d_rxc
 );
 
+// AXI between MAC and Ethernet modules
+wire [63:0] rx_axis_tdata;
+wire [7:0] rx_axis_tkeep;
+wire rx_axis_tvalid;
+wire rx_axis_tready;
+wire rx_axis_tlast;
+wire rx_axis_tuser;
+
+wire [63:0] tx_axis_tdata;
+wire [7:0] tx_axis_tkeep;
+wire tx_axis_tvalid;
+wire tx_axis_tready;
+wire tx_axis_tlast;
+wire tx_axis_tuser;
+
+eth_mac_10g_fifo #(
+    .ENABLE_PADDING(1),
+    .ENABLE_DIC(1),
+    .MIN_FRAME_LENGTH(64),
+    .TX_FIFO_ADDR_WIDTH(9),
+    .RX_FIFO_ADDR_WIDTH(9)
+)
+eth_mac_10g_fifo_inst (
+    .rx_clk(clk),
+    .rx_rst(rst),
+    .tx_clk(clk),
+    .tx_rst(rst),
+    .logic_clk(clk),
+    .logic_rst(rst),
+
+    .tx_axis_tdata(tx_axis_tdata),
+    .tx_axis_tkeep(tx_axis_tkeep),
+    .tx_axis_tvalid(tx_axis_tvalid),
+    .tx_axis_tready(tx_axis_tready),
+    .tx_axis_tlast(tx_axis_tlast),
+    .tx_axis_tuser(tx_axis_tuser),
+
+    .rx_axis_tdata(rx_axis_tdata),
+    .rx_axis_tkeep(rx_axis_tkeep),
+    .rx_axis_tvalid(rx_axis_tvalid),
+    .rx_axis_tready(rx_axis_tready),
+    .rx_axis_tlast(rx_axis_tlast),
+    .rx_axis_tuser(rx_axis_tuser),
+
+    .xgmii_rxd(sfp_a_rxd),
+    .xgmii_rxc(sfp_a_rxc),
+    .xgmii_txd(sfp_a_txd),
+    .xgmii_txc(sfp_a_txc),
+    
+    .rx_error_bad_frame(rx_error_bad_frame),
+    .rx_error_bad_fcs(rx_error_bad_fcs),
+
+    .ifg_delay(8'd12)
+);
+
+eth_axis_rx_64
+eth_axis_rx_inst (
+    .clk(clk),
+    .rst(rst),
+    // AXI input
+    .input_axis_tdata(rx_axis_tdata),
+    .input_axis_tkeep(rx_axis_tkeep),
+    .input_axis_tvalid(rx_axis_tvalid),
+    .input_axis_tready(rx_axis_tready),
+    .input_axis_tlast(rx_axis_tlast),
+    .input_axis_tuser(rx_axis_tuser),
+    // Ethernet frame output
+    .output_eth_hdr_valid(rx_eth_hdr_valid),
+    .output_eth_hdr_ready(rx_eth_hdr_ready),
+    .output_eth_dest_mac(rx_eth_dest_mac),
+    .output_eth_src_mac(rx_eth_src_mac),
+    .output_eth_type(rx_eth_type),
+    .output_eth_payload_tdata(rx_eth_payload_tdata),
+    .output_eth_payload_tkeep(rx_eth_payload_tkeep),
+    .output_eth_payload_tvalid(rx_eth_payload_tvalid),
+    .output_eth_payload_tready(rx_eth_payload_tready),
+    .output_eth_payload_tlast(rx_eth_payload_tlast),
+    .output_eth_payload_tuser(rx_eth_payload_tuser),
+    // Status signals
+    .busy(),
+    .error_header_early_termination()
+);
+
+eth_axis_tx_64
+eth_axis_tx_inst (
+    .clk(clk),
+    .rst(rst),
+    // Ethernet frame input
+    .input_eth_hdr_valid(tx_eth_hdr_valid),
+    .input_eth_hdr_ready(tx_eth_hdr_ready),
+    .input_eth_dest_mac(tx_eth_dest_mac),
+    .input_eth_src_mac(tx_eth_src_mac),
+    .input_eth_type(tx_eth_type),
+    .input_eth_payload_tdata(tx_eth_payload_tdata),
+    .input_eth_payload_tkeep(tx_eth_payload_tkeep),
+    .input_eth_payload_tvalid(tx_eth_payload_tvalid),
+    .input_eth_payload_tready(tx_eth_payload_tready),
+    .input_eth_payload_tlast(tx_eth_payload_tlast),
+    .input_eth_payload_tuser(tx_eth_payload_tuser),
+    // AXI output
+    .output_axis_tdata(tx_axis_tdata),
+    .output_axis_tkeep(tx_axis_tkeep),
+    .output_axis_tvalid(tx_axis_tvalid),
+    .output_axis_tready(tx_axis_tready),
+    .output_axis_tlast(tx_axis_tlast),
+    .output_axis_tuser(tx_axis_tuser),
+    // Status signals
+    .busy()
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 endmodule
